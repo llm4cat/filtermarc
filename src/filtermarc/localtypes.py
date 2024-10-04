96bf552eafe5df524360573f501ff837b3009171
Jason Thomale <jason.thomale@unt.edu>
2024-10-04 16:08:29 -0500

    Add local type definitions
    These are type definitions used locally throughout the package.

---
+++ src/filtermarc/localtypes.py
@@ -0,0 +1,28 @@
+"""Contains custom vars, protocols, etc. used for type hinting."""
+from collections.abc import Mapping, Sequence
+import os
+import sys
+from typing import Callable, Protocol, Union
+
+from pymarc import Field
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+
+# Misc type vars defined here.
+
+FieldSpec = Union[str, Sequence[str]]
+PathLike = Union[str, os.PathLike]
+RecordFilter = Callable[['RecordCacheLike'], bool]
+SubfieldSpec = Union[str, Sequence[str]]
+
+
+# Protocols defined here.
+
+class RecordCacheLike(Protocol):
+    """Protocol for types that are RecordCache-like."""
+    cache: Mapping[str, Sequence[Field]]
+    all_fields: Sequence[Field]

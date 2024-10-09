e6d01d05814342660bdf642704f391ca61849c93
Jason Thomale <jason.thomale@unt.edu>
2024-10-09 16:46:29 -0500

    Add imports to package __init__.py

--- src/filtermarc/__init__.py
+++ src/filtermarc/__init__.py
@@ -1,6 +1,15 @@
 """Tools for filtering MARC record data sets."""
 from importlib import metadata
 
+from . import localtypes, out, filters, formats, marc
+from .in_ import stream_records_from_files
+from .filters import RecordFilterPipeline
+from .out import Output, Job
+
 
 __version__ = metadata.version('filtermarc')
-__all__ = []
+__all__ = [
+    'localtypes', 'marc', 'filters', 'formats', 'out',
+    'stream_records_from_files', 'RecordFilterPipeline', 'filters', 'Output',
+    'Job'
+]
